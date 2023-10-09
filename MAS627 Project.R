cb6962300af0391e5d1caa5223da224d0409daeb
711mint <114817967+711mint@users.noreply.github.com>
2023-10-08 22:46:57 -0400

    Create visualizations + fluidRow layout
    added combined_data
    
    cleaned data
    
    add renderDataTable

--- MAS627 Project.R
+++ MAS627 Project.R
@@ -1,7 +1,3 @@
-#str_subset(ls('package:shiny'), 'render')
-#str_subset(ls('package:shiny'), 'Output')
-#str_subset(ls('package:shiny'), 'Input')
-
 
 ####### LIBRARIES ###########
 
@@ -18,7 +14,13 @@ library(purrr)
 
 ls("package:spotifyr")
 
-###############################
+#######
+
+#####TO-DO
+
+# Increase font size of x and y axis labels
+# change titles of visuals
+# explain what the audio features mean. + why observe them throu8gh densityß
 
 
 
@@ -30,7 +32,7 @@ client_secret = Sys.setenv(SPOTIFY_CLIENT_SECRET = '326eb15d679b4da29bbfe0c1e1fe
 access_token = get_spotify_access_token()
 
 
-###############################
+##########
 
 
 
@@ -55,60 +57,127 @@ combined_data <- bind_rows(
 )
 
 
-
-all_out_00s$track.album.artists[[1]][[3]]
-
-all_out_00s$extracted_data <- sapply(all_out_00s$track.album.artists, function(x) {
-  if(length(x) >= 3) x[[3]] else NA
-})
-
-all_out_00s$extracted_data
-
-
-####################################
+############
 
 
 ####### DATA CLEANING ###########
 
-####################################
+
+extract_artist_data <- function(data) {
+  data %>%
+    mutate(
+      artist_name = sapply(track.album.artists, function(x) {
+        if(length(x) >= 3) x[[3]] else NA
+      }),
+      combined_name = ifelse(lengths(artist_name) > 1, 
+                             sapply(artist_name, paste, collapse = " & "), 
+                             as.character(artist_name))
+    )
+}
+all_out_10s = extract_artist_data(all_out_10s)
+all_out_00s = extract_artist_data(all_out_00s)
+all_out_90s = extract_artist_data(all_out_90s)
+all_out_80s = extract_artist_data(all_out_80s)
+combined_data = extract_artist_data(combined_data)
+
+all_out_10s = all_out_10s %>%
+  select(
+    playlist_id,
+    playlist_name,
+    Danceability = danceability,
+    Energy = energy,
+    Loudness = loudness,
+    Speechiness = speechiness,
+    Acousticness = acousticness,
+    Instrumentalness = instrumentalness,
+    Liveness = liveness,
+    Valence = valence,
+    Tempo = tempo,
+    Track = track.name,
+    track.popularity,
+    track.album.name,
+    Artist = combined_name
+  )
+
+all_out_00s = all_out_00s %>%
+  select(
+    playlist_id,
+    playlist_name,
+    Danceability = danceability,
+    Energy = energy,
+    Loudness = loudness,
+    Speechiness = speechiness,
+    Acousticness = acousticness,
+    Instrumentalness = instrumentalness,
+    Liveness = liveness,
+    Valence = valence,
+    Tempo = tempo,
+    Track = track.name,
+    track.popularity,
+    track.album.name,
+    Artist = combined_name
+  )
+
+all_out_90s = all_out_90s %>%
+  select(
+    playlist_id,
+    playlist_name,
+    Danceability = danceability,
+    Energy = energy,
+    Loudness = loudness,
+    Speechiness = speechiness,
+    Acousticness = acousticness,
+    Instrumentalness = instrumentalness,
+    Liveness = liveness,
+    Valence = valence,
+    Tempo = tempo,
+    Track = track.name,
+    track.popularity,
+    track.album.name,
+    Artist = combined_name
+  )
+
+all_out_80s = all_out_80s %>%
+  select(
+    playlist_id,
+    playlist_name,
+    Danceability = danceability,
+    Energy = energy,
+    Loudness = loudness,
+    Speechiness = speechiness,
+    Acousticness = acousticness,
+    Instrumentalness = instrumentalness,
+    Liveness = liveness,
+    Valence = valence,
+    Tempo = tempo,
+    Track = track.name,
+    track.popularity,
+    track.album.name,
+    Artist = combined_name
+  )
+
+combined_data = combined_data %>%
+  select(
+    playlist_id,
+    playlist_name,
+    Danceability = danceability,
+    Energy = energy,
+    Loudness = loudness,
+    Speechiness = speechiness,
+    Acousticness = acousticness,
+    Instrumentalness = instrumentalness,
+    Liveness = liveness,
+    Valence = valence,
+    Tempo = tempo,
+    Track = track.name,
+    track.popularity,
+    track.album.name,
+    Artist = combined_name
+  )
 
 
-############ DATA VIZ - DENSITY x DANCEABILITY ########################
 
-
-all_out_00s %>%
-  group_by(extracted_data) %>%
-  summarise(song_count = n()) %>%
-  arrange(desc(song_count)) %>%
-  ggplot(aes(x = reorder(extracted_data, -song_count), y = song_count)) +  # Reorder artists by song count
-  geom_col() +
-  labs(title = "Number of Songs by Artist in All Out 00s Playlist",
-       x = "Artist",
-       y = "Number of Songs") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))  # Rotate x-axis labels for better readability
-
-
-
-############ DATA VIZ - DENSITY x DANCEABILITY ########################
-
-  playlist_audio_features %>% ggplot(aes(x=danceability, fill=playlist_name,
-                              text = paste(playlist_name)))+
-    geom_density(alpha=0.7, color=NA)+
-    scale_fill_manual(values=c('skyblue'))+
-    labs(x="Danceability", y="Density") +
-    guides(fill=guide_legend(title="Playlist"))+
-    theme_minimal()+
-    ggtitle("Distribution of Danceability Data") + 
-    geom_rug(alpha=0.5, sides="b") +
-    geom_vline(data= . %>% group_by(playlist_name) %>% 
-                   summarize(mean_danceability=mean(danceability)),
-                 aes(xintercept=mean_danceability), linetype="dashed", color="blue", alpha=0.7)
-  
-
-####################################
-
-
+#################################
 
 
 
@@ -117,22 +186,66 @@ all_out_00s %>%
 #########
 ui = fluidPage(
   
-  titlePanel("Density Visualization with Shiny"),
+  titlePanel("Decades of Music Visualization with Shiny"),
   
-  sidebarLayout(
+  tabsetPanel(
     
-    sidebarPanel(
-      selectInput("dataset", 
-                  "Choose a dataset:", 
-                  choices = c("all_out_80s", "all_out_90s", "all_out_00s", "all_out_10s"))
+    tabPanel('Top 10 Songs by Decade',
+             fluidRow(
+               column(4, selectInput("dataset2", 
+                                     "Choose a Spotify Playlist:", 
+                                     choices = c("All Out 80s" = "all_out_80s", 
+                                                 "All Out 90s" = "all_out_90s", 
+                                                 "All Out 00s" = "all_out_00s", 
+                                                 "All Out 10s" = "all_out_10s"),
+                                     selected = "all_out_80s")
+               )
+             ),
+             plotOutput("topArtistsPlot")
     ),
     
-    mainPanel(
-      plotOutput("densityPlot")
+    tabPanel('Density Plots by Track Audio Features',
+             fluidRow(
+               column(4, selectInput("dataset1", 
+                                     "Choose a Spotify Playlist:", 
+                                     choices = c("All Out 80s" = "all_out_80s", 
+                                                 "All Out 90s" = "all_out_90s", 
+                                                 "All Out 00s" = "all_out_00s", 
+                                                 "All Out 10s" = "all_out_10s",
+                                                 "1980s to 2010s" = "combined_data"),
+                                     selected = "all_out_80s")
+               ),
+               column(4, selectInput('xvar', 
+                                     'X-Axis Variable', 
+                                     choices=c('Danceability', 
+                                               'Energy', 
+                                               'Loudness',
+                                               'Speechiness',
+                                               "Acousticness",
+                                               "Liveness",
+                                               "Valence",
+                                               "Tempo"), 
+                                     selected='Danceability')),
+             ),
+             plotOutput("densityPlot")
+    ),
+    
+    tabPanel('Playlist Data',
+             fluidRow(
+               column(4, selectInput("dataset3", 
+                                     "Choose a Spotify Playlist:", 
+                                     choices = c("All Out 80s" = "all_out_80s", 
+                                                 "All Out 90s" = "all_out_90s", 
+                                                 "All Out 00s" = "all_out_00s", 
+                                                 "All Out 10s" = "all_out_10s",
+                                                 "1980s to 2010s" = "combined_data"),
+                                     selected = "all_out_80s")
+             ),
+             dataTableOutput("source_data")
     )
-  ) #closes sidebar layout
+    )))
+
   
-)
 
 
 #################
@@ -141,30 +254,78 @@ ui = fluidPage(
 
 server <- function(input, output) {
   
-  output$densityPlot <- renderPlot({
-    # Depending on input, select dataset
-    
-    data_selected <- switch(input$dataset,
+  
+  # Artist with Most Popular Songs
+  output$topArtistsPlot <- renderPlot({
+
+    data_selected <- switch(input$dataset2,
                             "all_out_80s" = all_out_80s,
                             "all_out_90s" = all_out_90s,
                             "all_out_00s" = all_out_00s,
                             "all_out_10s" = all_out_10s)
     
-    # Create plot
-    ggplot(data_selected ,aes(x=danceability, fill=playlist_name,
-                                           text = paste(playlist_name)))+
-      geom_density(alpha=0.7, color=NA)+
-      labs(x="Danceability", y="Density") +
-      guides(fill=guide_legend(title="Playlist"))+
-      theme_minimal()+
-      ggtitle("Distribution of Danceability Data") + 
-      geom_rug(alpha=0.5, sides="b") +
-      geom_vline(data= . %>% group_by(playlist_name) %>% 
-                   summarize(mean_danceability=mean(danceability)),
-                 aes(xintercept=mean_danceability), linetype="dashed", color="blue", alpha=0.7)
+    
+    data_selected %>%
+      group_by(Track) %>%
+      summarise(avg_popularity = mean(track.popularity, na.rm = TRUE)) %>%
+      arrange(desc(avg_popularity)) %>%
+      slice_head(n = 10) %>%
+      ggplot(aes(x =avg_popularity, y = reorder(Track, avg_popularity))) +  # Reorder artists by popularity
+      geom_segment(aes(x = 80, xend = avg_popularity, yend = reorder(Track, avg_popularity)), color = "gray") +
+      geom_point() +
+      labs(title = "Top 10 Songs by Decade",
+           x = "Popularity Index (Scale: 0-100, Displayed: 80-100",
+           y = "Song") +
+      theme_minimal() +
+      theme(
+        axis.text.x = element_text(angle = 45, hjust = 1, size = 16),
+        axis.text.y = element_text(size = 16)
+      ) 
+    
   })
   
+  # user defined density plot
+  output$densityPlot <- renderPlot({
+    
+    data_selected <- switch(input$dataset1,
+                            "all_out_80s" = all_out_80s,
+                            "all_out_90s" = all_out_90s,
+                            "all_out_00s" = all_out_00s,
+                            "all_out_10s" = all_out_10s,
+                            "combined_data" = combined_data)
+    
+
+    ggplot(data_selected ,aes(x=!!sym(input$xvar), fill=playlist_name,
+                                           text = paste(playlist_name)))+
+      geom_density(alpha=0.7, color=NA)+
+      labs(x=input$xvar, y="Density") +
+      guides(fill=guide_legend(title="Playlist"))+
+      theme_minimal()+
+      theme(
+        axis.text.x = element_text(angle = 45, hjust = 1, size = 16),
+        axis.text.y = element_text(size = 16)
+      ) +
+      ggtitle("Distribution of Danceability Data")
+    
+    
+  })
+  
+  # output source data
+  output$source_data <- renderDataTable({
+    switch(input$dataset3,
+           "all_out_80s" = all_out_80s,
+           "all_out_90s" = all_out_90s,
+           "all_out_00s" = all_out_00s,
+           "all_out_10s" = all_out_10s,
+           "combined_data" = combined_data)
+  })
+    
+  
+  
+
+  
 }
+
 
 
 
